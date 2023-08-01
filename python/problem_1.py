0b1ddbb9f51fd869cd61afa25542c50883bb85f8
Leo Riviera <leo.riviera@voxa.dev>
2023-08-01 16:52:15 +0100

    Update problems

--- python/problem_1.py
+++ python/problem_1.py
@@ -1,5 +1,4 @@
 def problem_1():
-    
     "Find the sum of all the multiples of 3 or 5 below 1000."
     total = 0
 
--- python/problem_45.py
+++ python/problem_45.py
@@ -1,15 +1,15 @@
 def problem_45():
     "Find the next triangle number [after 40755] that is also pentagonal and hexagonal."
 
-    def triangleNGenerator(n):
+    def triangle_n_generator(n):
         t_n = n * (n + 1) / 2
         return t_n
 
-    def pentagonNGenerator(n):
+    def pentagon_n_generator(n):
         p_n = n * (3 * n - 1) / 2
         return p_n
 
-    def hexagonNGenerator(n):
+    def hexagon_n_generator(n):
         h_n = n * (2 * n - 1)
         return h_n
 
@@ -20,9 +20,9 @@ def problem_45():
     hexagon_n_list = []
 
     while True:
-        triangle_n = triangleNGenerator(count)
-        pentagon_n = pentagonNGenerator(count)
-        hexagon_n = hexagonNGenerator(count)
+        triangle_n = triangle_n_generator(count)
+        pentagon_n = pentagon_n_generator(count)
+        hexagon_n = hexagon_n_generator(count)
 
         triangle_n_list.append(triangle_n)
         pentagon_n_list.append(pentagon_n)
